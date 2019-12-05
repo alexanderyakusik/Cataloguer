3c5d6cd350357d80e613ce45c608ffe9220e28e2
Alexander Yakusik <c-ayakusik@neogov.net>
2019-12-05 17:58:18 +0300

    Added sample UI project.

---
+++ Cataloguer.UI/Cataloguer.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Cataloguer.UI
+{
+    public partial class Cataloguer : Form
+    {
+        public Cataloguer()
+        {
+            InitializeComponent();
+        }
+    }
+}
